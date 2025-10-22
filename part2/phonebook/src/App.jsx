20edea9da82f3ecc00a5084a34f2cf7fe4552ffe
dolopichtis <dolopichtis@gmail.com>
2025-10-22 06:35:22 +0300

    add filtering, ex.2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,11 +2,16 @@ import { useState } from 'react'
 import  Contact from './component/Contact'
 
 const App = () => {
-	const [id, setId] = useState(1);
+	const [id, setId] = useState(4);
 	const [contacts, setContacts] = useState(
-		[{ id: id, name: 'Arto Hellas', number: 12345 }]
+		[
+			{ name: 'Arto Hellas', number: '040-123456', id: 1 },
+			{ name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+			{ name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+		]
 	)
-	const [newContact, setNewContact] = useState(''); 
+	const [finded, setFinded] = useState(contacts);
+	const [newContact, setNewContact] = useState(); 
 	const handleInputName = (input) => {
 		setNewContact({...newContact, name: input.target.value});
 	}
@@ -22,18 +27,22 @@ const App = () => {
 			const newId = id + 1;
 			setId(newId);
 			setContacts([...contacts, {...newContact, id: newId}]);
+			setFinded([...contacts, {...newContact, id: newId}]);
 		}
+	}
+	const handleFind = (input) => {
+		setFinded(contacts.filter(contact => contact.name.toLowerCase().startsWith(input.target.value)));
 	}
 
 	return (
 		<>
 			<form onSubmit= { handleSubmit} >
-				<p> debug: {newContact.name}</p>
+				<p> find: <input name='find' onChange = {handleFind} /> </p>
 				<p> name: <input name='newContact' onChange= {handleInputName} /> </p>
 				<p> phone: <input name='phone' onChange= {handleInputPhone} /> </p>
 				<button type='submit' >add</button>
 			</form>
-			{contacts.map(
+			{finded.map(
 				(contact) => <Contact key={contact.id} contact={contact} />)
 			} 
 		</>
