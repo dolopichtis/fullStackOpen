41e06a2f5b846288c17f3229b200de528ed20c6f
dolopichtis <dolopichtis@gmail.com>
2025-12-09 21:39:04 +0200

    part 3.2, info page

--- part3/phonebookServer/simpleServer.js
+++ part3/phonebookServer/simpleServer.js
@@ -1,32 +1,38 @@
 const express = require('express');
 
 const phonebook = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
+	{ 
+		"id": "1",
+		"name": "Arto Hellas", 
+		"number": "040-123456"
+	},
+	{ 
+		"id": "2",
+		"name": "Ada Lovelace", 
+		"number": "39-44-5323523"
+	},
+	{ 
+		"id": "3",
+		"name": "Dan Abramov", 
+		"number": "12-43-234345"
+	},
+	{ 
+		"id": "4",
+		"name": "Mary Poppendieck", 
+		"number": "39-23-6423122"
+	}
 ];
 
 const simplePhonebook = express();
 
+simplePhonebook.get('/info', (req, res) => {
+	const reqTime = new Date();
+	res.send(`<p>Phonebook has data for ${phonebook.length} people</p>
+<p>Request time is ${reqTime}</p>`);
+});
+
 simplePhonebook.get('/api/persons', (request, response) => {
-response.json(phonebook);
+	response.json(phonebook);
 });
 
 const PORT = 3001;
